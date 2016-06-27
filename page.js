12b6a53dc885715d33eb02c4819966e9b242fcaf
Nate Fox <nate.m.fox@gmail.com>
2016-06-27 07:17:26 -0700

    0.1.6

--- page.js
+++ page.js
@@ -121,5 +121,5 @@ function get_windows_user() {
 }
 
 function get_selector(row,div) {
-    return $("table.OJ > tbody tr:eq("+row+") div.AK:eq("+div+")")
+    return $("table.PJ > tbody tr:eq("+row+") div.BK:eq("+div+")")
 }
